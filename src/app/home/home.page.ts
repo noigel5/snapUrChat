bf653e8f9471b6007f5437dfc4a2730af5c937ea
noigel5 <ffiechter@gmx.ch>
2023-11-16 15:59:13 +0100

    initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    FormsModule],
+})
+export class HomePage {
+  selectionDayNight: string;
+  constructor() {
+    this.selectionDayNight = "light"
+  }
+}
