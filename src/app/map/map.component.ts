4d0f5bed3913f0f0a50e4da998bf52ae36005722
noigel5 <ffiechter@gmx.ch>
2023-11-24 10:24:20 +0100

    map (not working)

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,41 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { IonicModule } from '@ionic/angular'
 import { FormsModule } from '@angular/forms'
-import { GeolocatorService } from '../services/geolocator.service';
+import { GoogleMap } from '@capacitor/google-maps';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-map',
   templateUrl: 'map.component.html',
   styleUrls: ['map.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   standalone: true,
   imports: [
     IonicModule,
     FormsModule
   ]
 })
-export class MapComponent  implements OnInit {
+export class MapComponent implements AfterViewInit {
+  @ViewChild('map')
 
-  latitude : number = 0
-  longitude : number = 0
-  altitude : number | null = 0
+  mapRef!: ElementRef<HTMLElement>;
+  newMap!: GoogleMap;
 
-  constructor( public geolocationService : GeolocatorService) {  }
-
-  getCurrentPosition = async () => {
-    const position = await this.geolocationService.getCurrentPosition()
-
-    this.latitude = position.coords.latitude
-    this.longitude = position.coords.longitude
-    this.altitude = position.coords.altitude
+  async createMap() {
+    this.newMap = await GoogleMap.create({
+      id: 'my-cool-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: 33.6,
+          lng: -117.9,
+        },
+        zoom: 8,
+      },
+    });
   }
 
-  resetPosition () {
-    this.latitude = 0
-    this.longitude = 0
-    this.altitude = 0
+  ngAfterViewInit() {
+    this.createMap();
   }
 
-  ngOnInit() {
-    this.getCurrentPosition()
-  }
+  constructor() { }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://rsfjzoarcbrwoppspfst.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJzZmp6b2FyY2Jyd29wcHNwZnN0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwMDE0NjA3NiwiZXhwIjoyMDE1NzIyMDc2fQ.reftsHUBuy8M-x1lH5srsedMD3iU6wNJl7yN8KZHz04',
+  mapsKey: 'AIzaSyDb8evRmJ7vRD979KDtz2xiXfe4YfveYa8'
 };
